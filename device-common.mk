5d234dfead5b9f60a3e6a27097129cdeb5a23eb4
Danny Lin <danny@kdrag0n.dev>
2020-10-30 12:57:58 -0700

    floral: Use EXPENSIVE_RENDERING power hint for blurs
    We need to boost the GPU for smooth high-radius blurs as blurring is an
    expensive rendering job.
    
    Change-Id: Ic511273b2dce637de0796be9b59b91dd0b3a16e0

--- device-common.mk
+++ device-common.mk
@@ -133,3 +133,4 @@ PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
+    ro.sf.blurs_are_expensive=1
